d093491ba67d97ce26798e58257d31602f04955c
Arunabha Mitra <arunabha.official@gmail.com>
2021-08-10 01:06:57 +0530

    Add files via upload

---
+++ DecypherString/DecypherString/ManageDecyphering.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecypherString
+{
+    class ManageDecyphering
+    {
+        public static string DecypherEncodedString(string keyWrd, string cypher)
+        {
+            string msg = "";
+            string alphabets = "abcdefghijklmnopqrstuvwxyz";
+            Dictionary<char, char> keyCypherMap = new Dictionary<char, char>();
+            for(int i = 0; i < alphabets.Length; i++)
+                keyCypherMap.Add(cypher[i], alphabets[i]);
+            
+            foreach (char keyWordChar in keyWrd)
+                msg += cypher.Contains(keyWordChar.ToString().ToLower()) ? 
+                            (  Char.IsLower(keyWordChar) ? 
+                                        keyCypherMap[keyWordChar] 
+                                                : Char.ToUpper(keyCypherMap[Char.ToLower(keyWordChar)])
+                            ) : (keyWordChar == '_' ? ' ' : keyWordChar);
+
+            return msg;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decyphered ==>> " + DecypherEncodedString("Z:be_p,I", "bdachgfelkjiponmtsrqxwvuzy"));
+        }
+    }
+}
